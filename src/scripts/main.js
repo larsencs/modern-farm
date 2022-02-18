d658ef71bb76501c98016e4e5ac621841d516c9e
larsencs <quietus77@gmail.com>
2022-02-18 16:34:54 -0600

    Added tractor.js and harvester.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,22 @@
 console.log("Welcome to the main module")
 
 import { createPlan } from './plan.js';
-import {createCorn} from "./seeds/corn.js";
-import {createSunflower} from "./seeds/sunflower.js";
-import {createSoybean} from "./seeds/soybean.js";
-import {createAsparagus} from "./seeds/asparagus.js";
-import {createWheat} from "./seeds/wheat.js";
-import {createPotato} from "./seeds/potato.js";
-import {addPlant} from "./field.js";
-import {usePlants} from "./field.js";
+// import {createCorn} from "./seeds/corn.js";
+// import {createSunflower} from "./seeds/sunflower.js";
+// import {createSoybean} from "./seeds/soybean.js";
+// import {createAsparagus} from "./seeds/asparagus.js";
+// import {createWheat} from "./seeds/wheat.js";
+// import {createPotato} from "./seeds/potato.js";
+// import {addPlant} from "./field.js";
+// import {usePlants} from "./field.js";
+import {plantSeeds} from "./tractor.js";
 
-addPlant(createSunflower())
-console.log(usePlants())
-addPlant(createCorn())
-console.log(usePlants())
+console.log(plantSeeds(createPlan()))
+
+// addPlant(createSunflower())
+// console.log(usePlants())
+// addPlant(createCorn())
+// console.log(usePlants())
 
 // const yearlyPlan = createPlan()
 // const cornTester = createCorn()
