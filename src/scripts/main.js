1875ae31b10dc689917182da461986cf05621f8a
larsencs <quietus77@gmail.com>
2022-02-18 14:48:07 -0600

    Added tilling javascript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,21 +7,28 @@ import {createSoybean} from "./seeds/soybean.js";
 import {createAsparagus} from "./seeds/asparagus.js";
 import {createWheat} from "./seeds/wheat.js";
 import {createPotato} from "./seeds/potato.js";
+import {addPlant} from "./field.js";
+import {usePlants} from "./field.js";
 
-const yearlyPlan = createPlan()
-const cornTester = createCorn()
-const sunflowerTester = createSunflower()
-const soybeanTester = createSoybean()
-const asparagusTester = createAsparagus()
-const wheatTester = createWheat()
-const potatoTester = createPotato()
+addPlant(createSunflower())
+console.log(usePlants())
+addPlant(createCorn())
+console.log(usePlants())
 
-console.log(yearlyPlan)
-console.log(sunflowerTester)
-console.log(soybeanTester)
-console.log(asparagusTester)
-console.log(wheatTester)
-console.log(potatoTester)
-console.log(cornTester)
+// const yearlyPlan = createPlan()
+// const cornTester = createCorn()
+// const sunflowerTester = createSunflower()
+// const soybeanTester = createSoybean()
+// const asparagusTester = createAsparagus()
+// const wheatTester = createWheat()
+// const potatoTester = createPotato()
+
+// console.log(yearlyPlan)
+// console.log(sunflowerTester)
+// console.log(soybeanTester)
+// console.log(asparagusTester)
+// console.log(wheatTester)
+// console.log(potatoTester)
+// console.log(cornTester)
 
 
